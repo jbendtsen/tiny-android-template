d7ac0d5b3977efef06875e3848e4c320246438d1
Omar Azmi <64020006+omar-azmi@users.noreply.github.com>
2024-03-29 09:59:50 -0400

    modify `include.sh` so that it is compatible with windows paths and commands.
    - rename `$CMD_COPY` to `$CMD_COPY_RECURSIVE` to better reflect its purpose.
    - `API_LEVEL` had to be set to lower than `29` because there seems to be a compilation issue with `29` and above. see java.lang.ClassNotFoundException: android.app.Application$ActivityLifecycleCallbacks$-CC #4

--- includes.sh
+++ includes.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Roughly arranged in descending order of how likely you'll need to change each value
 
-HOST_OS="linux-x86_64"
-API_LEVEL="30"
-ANDROID_VERSION="11"
-NDK_VERSION="r23-beta4"
+HOST_OS="windows-x86_64" # use "windows-x86_64" for windows, and "linux-x86_64" for linux
+API_LEVEL="28"
+ANDROID_VERSION="10"
+NDK_VERSION="23.0.7599858"
 TARGET_ARCHES=( "arm64-v8a" )
-SDK_DIR="../Sdk"
+SDK_DIR="C:/Android/sdk"
 KOTLIN_LIB_DIR="/usr/share/kotlin/lib"
 
 REPO="https://dl.google.com/dl/android/maven2"
@@ -14,10 +14,10 @@ REPO="https://dl.google.com/dl/android/maven2"
 KEYSTORE="keystore.jks"
 KS_PASS="123456"
 
-TOOLS_DIR="$SDK_DIR/android-$ANDROID_VERSION"
-PLATFORM_DIR="$SDK_DIR/android-$ANDROID_VERSION"
+TOOLS_DIR="$SDK_DIR/build-tools/28.0.3"
+PLATFORM_DIR="$SDK_DIR/platforms/android-28"
 
-NDK_DIR="$SDK_DIR/android-ndk-$NDK_VERSION"
+NDK_DIR="$SDK_DIR/ndk/${NDK_VERSION}"
 NDK_BIN_DIR="$NDK_DIR/toolchains/llvm/prebuilt/$HOST_OS/bin"
 NDK_INCLUDE_DIR="$NDK_DIR/toolchains/llvm/prebuilt/$HOST_OS/usr/sysroot/include"
 NDK_LIB_DIR="$NDK_DIR/toolchains/llvm/prebuilt/$HOST_OS/usr/sysroot/lib"
@@ -30,10 +30,12 @@ PKG_OUTPUT="lib"
 JAR_TOOLS="java -Xmx1024M -Xss1m -jar $TOOLS_DIR/lib"
 
 CMD_7Z="7z"
-CMD_MKDIR="mkdir"
-CMD_RENAME="mv"
-CMD_DELETE="rm -rf"
-CMD_FIND="/usr/bin/find"
+CMD_MKDIR="mkdir" # use "mkdir" for windows, and "mkdir" for linux
+CMD_RENAME="ren" # use "ren" for windows, and "mv" for linux
+CMD_COPY_RECURSIVE="xcopy" # use "xcopy" for windows, and "cp -r" for linux
+CMD_DELETE="rmdir /s /q" # use "rmdir /s /q" for windows, and "rm -rf" for linux
+CMD_FIND_SRC_JAVA="ls -r src/**.java" # use "ls -r src/**.java" for windows, and "usr/bin/find src -name \"*.java\"" for linux
+CMD_FIND_SRC_KOTLIN="ls -r src/**.kt" # use "ls -r src/**.kt" for windows, and "usr/bin/find src -name \"*.kt\"" for linux
 CMD_CURL="curl"
 CMD_SED="sed"
 CMD_JAR="jar"
@@ -43,3 +45,5 @@ CMD_KOTLINC="kotlinc"
 
 CMD_ADB="$SDK_DIR/platform-tools/adb"
 CMD_D8="$CMD_JAVA -Xmx1024M -Xss1m -cp $TOOLS_DIR/lib/d8.jar com.android.tools.r8.D8"
+
+DEV_NULL="NUL" # use "NUL" for windows, and "/dev/null" for linux
